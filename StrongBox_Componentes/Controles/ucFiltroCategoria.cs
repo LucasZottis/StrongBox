0895ec7fac0c7cca70c615a0c11d90d0aac7236c
LucasZottis <zottis.lucas@hotmail.com>
2021-03-18 07:03:42 -0300

    Inclusão de um novo controle.

---
+++ StrongBox_Componentes/Controles/ucFiltroCategoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StrongBox_Componentes.Controles {
+    public partial class ucFiltroCategoria : UserControl {
+        public ucFiltroCategoria() {
+            InitializeComponent();
+        }
+    }
+}
