0442986cf5c9256205120dc1920330f931de3b86
lucas.souza <zottis.lucas@hotmail.com>
2021-03-18 13:12:05 -0300

    Finalizado a criação do componente

--- StrongBox_Componentes/Controles/ucFiltroCategoria.Designer.cs
+++ StrongBox_Componentes/Controles/ucFiltroCategoria.Designer.cs
@@ -24,10 +24,72 @@ namespace StrongBox_Componentes.Controles {
         /// o conteúdo deste método com o editor de código.
         /// </summary>
         private void InitializeComponent() {
-            components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.cbxCategoria = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(7, 24);
+            this.txtCodigo.MaxLength = 2;
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(52, 22);
+            this.txtCodigo.TabIndex = 1;
+            this.txtCodigo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // cbxCategoria
+            // 
+            this.cbxCategoria.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbxCategoria.FormattingEnabled = true;
+            this.cbxCategoria.Location = new System.Drawing.Point(66, 24);
+            this.cbxCategoria.Name = "cbxCategoria";
+            this.cbxCategoria.Size = new System.Drawing.Size(180, 24);
+            this.cbxCategoria.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Categoria";
+            // 
+            // ucFiltroCategoria
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbxCategoria);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "ucFiltroCategoria";
+            this.Size = new System.Drawing.Size(251, 52);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ucFiltroCategoria_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.ComboBox cbxCategoria;
+        private System.Windows.Forms.Label label2;
     }
 }
--- StrongBox_Componentes/Controles/ucFiltroCategoria.cs
+++ StrongBox_Componentes/Controles/ucFiltroCategoria.cs
@@ -1,17 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using StrongBox.Biblioteca.Classes.Estaticas;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace StrongBox_Componentes.Controles {
+namespace StrongBox_Componentes.Controles
+{
     public partial class ucFiltroCategoria : UserControl {
+        #region Propriedades e Atributos
+
+        public DataTable FonteDados 
+        { 
+            private get 
+            { 
+                return FonteDados; 
+            } 
+            set 
+            { 
+                FonteDados = value; 
+            } 
+        }
+
+        public int Categoria 
+        { 
+            get 
+            { 
+                return cbxCategoria.SelectedValue.ParaInt(); 
+            } 
+
+            set
+            {
+                Categoria = txtCodigo.Text.ParaInt();
+            }
+        }
+
+        #endregion
+
+        #region Construtores
+
         public ucFiltroCategoria() {
             InitializeComponent();
         }
+
+
+        #endregion
+
+        #region Eventos
+
+        private void ucFiltroCategoria_KeyDown( object sender, KeyEventArgs e )
+        {
+            if (e.KeyCode == Keys.F3)
+            {
+                if (string.IsNullOrEmpty(txtCodigo.Text))
+                {
+                    cbxCategoria.DataSource = FonteDados;
+                }
+                else
+                {
+                    cbxCategoria.DataSource = FonteDados.Select( $"CODIGO = {txtCodigo.Text}" );
+                }
+            }
+        }
+
+        #endregion
     }
 }
